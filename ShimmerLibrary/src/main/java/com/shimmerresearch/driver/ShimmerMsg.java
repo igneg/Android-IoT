ab6531ca8e7b9787c4efac4238c580003b78b7c5
Gengi <gengi@tesseract.ch>
2016-09-05 08:08:44 +0200

    Initial commit: adds general structure

---
+++ ShimmerLibrary/src/main/java/com/shimmerresearch/driver/ShimmerMsg.java
@@ -0,0 +1,10 @@
+package com.shimmerresearch.driver;
+public class ShimmerMsg {
+	public int mIdentifier;
+	public Object mB;
+	
+	public ShimmerMsg(int a, Object b){
+		mIdentifier=a;
+		mB=b;
+	}
+}
